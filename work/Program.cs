a0ea0cf601e1a7442173df0c07b92670ee64b289
marina vorushina <butorinamarina@gmail.com>
2023-01-10 21:22:58 +0500

    add itog word

---
+++ work/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//Решение задачи - нахождение суммы четных чисел
+
+int number = 4;
+int sum = 0;
+for (int i = 0; i <= number; i++)
+{
+     i += 2;
+     sum = i + sum;
+           
+}
+Console.Write($"{sum} ");
+
